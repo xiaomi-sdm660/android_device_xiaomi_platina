918d7b20382afc6057ec44a5af6614c4f6bf63ee
Max Weffers <rcstar6696@gmail.com>
2019-11-02 16:47:11 +0100

    wayne: Move Build fingerprint to common

--- lineage_wayne.mk
+++ lineage_wayne.mk
@@ -34,8 +34,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRIVATE_BUILD_DESC="wayne-user 9 PKQ1.180904.001 V10.3.4.0.PDCCNXM release-keys"
 
-BUILD_FINGERPRINT="xiaomi/wayne/wayne:9/PKQ1.180904.001/V10.3.4.0.PDCCNXM:user/release-keys"
-
 # Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
