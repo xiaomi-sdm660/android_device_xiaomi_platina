fa8321f82944269b7ccb84cb4327bcdafcee7035
Max Weffers <rcstar6696@gmail.com>
2020-01-02 14:25:15 +0100

    wayne: Treewide: Cleanup and Reorder
    * Remove Double Definition
    * Reordeder Alphabetic
    * Move Audio and Media Files to Configs
    * Move sensor files to configs
    * Reorder Properties
    * Fix whitespaces

--- carbon_wayne.mk
+++ carbon_wayne.mk
@@ -35,7 +35,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Build Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRIVATE_BUILD_DESC="wayne-user 9 PKQ1.180904.001 V10.3.4.0.PDCCNXM release-keys"
+    PRIVATE_BUILD_DESC="wayne-user 9 PKQ1.180904.001 V10.3.4.0.PDCCNXM release-keys"
 
 # CarbonRom Maintainer
 PRODUCT_PROPERTY_OVERRIDES += \
