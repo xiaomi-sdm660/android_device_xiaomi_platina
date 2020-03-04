55bf1d4acfb581fd5a3a63e3201b256dd59eb896
nysascape <nysadev@raphielgang.org>
2020-03-04 12:12:17 +0000

    props: Set DPI to MIUI
    Signed-off-by: nysascape <nysadev@raphielgang.org>
    Change-Id: Ia7e8fed7fb791cef74fd58dbb885320be0ccc62e

--- device_prop.mk
+++ device_prop.mk
@@ -100,7 +100,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # DPI
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.display.lcd_density=403
+    ro.sf.lcd_density=392
+    vendor.display.lcd_density=392
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
