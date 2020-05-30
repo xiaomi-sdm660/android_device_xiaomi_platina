1da07e0b4d27f493628997680a46f21f48fbf330
ricksanchez777 <henripaz1998@gmail.com>
2020-05-30 12:00:50 +0000

    platina: remove compiler

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,8 +32,6 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/wake_gesture"
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/platina
 TARGET_KERNEL_CONFIG := platina_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-elf-
-KERNEL_TOOLCHAIN := $(PWD)/compiler/bin/
 
 # Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
